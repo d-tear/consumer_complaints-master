45910971065d2da5bab75c0dbbf88124905def70
David Tyrpak <tyrpak@usc.edu>
2020-07-22 12:43:52 -0700

    Update consumer_complaints.py
    calculate the highest (rounded) percentage duplication in a given list. Also finished logic for change of product within a given year

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -10,6 +10,38 @@ import csv
 
 import sys
 
+from collections import Counter
+
+
+def max_percentage(input_list):
+    """
+    calculate the highest (rounded) percentage duplication in a given list
+    
+    e.g. ["a", "a", "b"] -> 66
+    
+    Arguments
+    ---------------------------
+    input_list : list type
+    
+    
+    
+    Returns
+    --------------------------
+    
+    the highest (rounded) percentage duplication, int type (the percentage is rounded using standard conventions)
+    (i.e., Any percentage between 0.5% and 1%, inclusive, should round to 1% and anything less than 0.5% should round to 0%)
+    """
+
+    count = Counter(input_list).items()
+
+    #a dictionary with key = entry, and value = the percentage of times that entry appears in the list
+    percentages = {x: round(float(y) / len(input_list) * 100) for x, y in count}
+    
+    
+    return max(percentages.values())
+
+
+
 def generate_report(path_to_csv, output_dir):
     
    """ 
@@ -54,13 +86,14 @@ def generate_report(path_to_csv, output_dir):
    
    row_counter = 0 #use this to loop through each OrderedDict/row 
    
-   product_complaints_per_year = 0 #total number of complaints received for that product and year
+   product_complaint_counter = 0 #total number of complaints received for that product and year
    
    at_least_one_complaint = set() #total number of companies receiving at least one complaint for that product and year
    
    bad_companies = [] # add a company to this list each time find a complaint for the given year and given product. 
    #The worst company is the one repeated the most in this list
    
+   #iterate through each row of the dataset
    while row_counter < len(sorted_data):
        
        company = sorted_data[row_counter]['Company']
@@ -69,13 +102,13 @@ def generate_report(path_to_csv, output_dir):
        
        product = sorted_data[row_counter]['Product']
        
-       
+       #if the year hasnt changed, we are still  need to summarize product data for the current year
        if year == current_year:
            
-           #if year and product are the same, update our metrics for that year and that product
+           #if year and product are the same, update our metrics for that product for this year
            if product == current_product:
                
-               product_complaints_per_year += 1
+               product_complaint_counter += 1
                
                
                #append the company to the list of bad companies for that product and year
@@ -84,9 +117,37 @@ def generate_report(path_to_csv, output_dir):
                if company not in at_least_one_complaint:
                    
                    at_least_one_complaint.add(company)
-       
+               else:
+                   pass
+            
+           #if we make it here, we have iterated through all rows of the current product for the current year.
+           #so we need to create the row data for this product of this year, AND update our product collection variables
+           #We DONT need to update the year
+           else:
+               row_data = [current_product, current_year, product_complaint_counter, len(at_least_one_complaint),max_percentage(bad_companies)]
+               
+               #append our inner list to summarized data nest list
+               summarized_data.append(row_data)
+               
+               ##Now we need to update our product collection variables.##
+               
+               #update the current product
+               current_product = sorted_data[row_counter]['Product']
+               
+               #clear our list of bad companies
+               bad_companies.clear()
+               
+               #reset our number of product complaints to zero
+               product_complaint_counter = 0
+               
+               #and clear our set of companies that had at least one complaint
+               at_least_one_complaint.clear()
+               
+               
+    
+              
         
-       row_counter += 1
+       # row_counter += 1
     
                
                

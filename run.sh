37c00161cb1118a557951ff74d9021b6e9cbaf65
David Tyrpak <tyrpak@usc.edu>
2020-07-24 16:44:54 -0700

    Update run.sh
    removed bad grammar

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #
 # You can use this shell script to generate the ./output/results.csv file
 # Note that this bash script assumes you are currently inside the top level directory
-# and it assumes you have a file named complaints.csv located inside ./input directory
+# and it assumes you have a file named complaints.csv located inside ./input 
 
 python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/results.csv

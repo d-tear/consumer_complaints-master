14e68f9c6b15e4ea14029d562ad2408a8927a247
David Tyrpak <tyrpak@usc.edu>
2020-07-26 10:29:38 -0700

    Update run.sh
    changed output file from results.csv to report.csv, which is what it is supposed to be!

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # Note that this bash script assumes you are currently inside the top level directory
 # and it assumes you have a file named complaints.csv located inside ./input 
 
-python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/results.csv
+python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
